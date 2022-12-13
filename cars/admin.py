5076ca87b30a8835fa2572b6d5dcd95607b536e8
Nassr-Allah Guetatlia <59767243+Nassr-Allah@users.noreply.github.com>
2022-12-13 14:00:24 +0100

    altered models

--- cars/admin.py
+++ cars/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import CarModel, Car, Service, Reservation, Price
+from .models import *
 
 # Register your models here.
 admin.site.register(CarModel, admin.ModelAdmin)
 admin.site.register(Car, admin.ModelAdmin)
 admin.site.register(Service, admin.ModelAdmin)
 admin.site.register(Reservation, admin.ModelAdmin)
-admin.site.register(Price, admin.ModelAdmin)
+admin.site.register(MiniService, admin.ModelAdmin)
